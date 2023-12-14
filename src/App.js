f082aedd4281f571a53cbfd7d5746bd9a535fd71
SeanMCameron <SeanCameronDev@gmail.com>
2023-12-14 16:31:04 -0500

    adding making request secure

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <Navbar> </Navbar>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/resume-web/about" element={<About />} />
+          <Route path="/resume-web/contact" element={<Contact />} />
+          <Route path="/resume-web/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
