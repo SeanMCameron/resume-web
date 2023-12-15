5c19ec39befa61daae7630b211362281ba6e8725
SeanMCameron <SeanCameronDev@gmail.com>
2023-12-15 12:26:59 -0500

    added path

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
       <HashRouter>
         <Navbar> </Navbar>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="resume-web/" element={<Home />} />
+          <Route path="resume-web/about" element={<About />} />
+          <Route path="resume-web/contact" element={<Contact />} />
+          <Route path="resume-web/dashboard" element={<Dashboard />} />
         </Routes>
       </HashRouter>
     </div>
